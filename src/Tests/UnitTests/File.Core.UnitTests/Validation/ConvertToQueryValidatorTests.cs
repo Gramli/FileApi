1982b95ce7b6094077ac0db7e71c123425d45d76
Gramli <Gramli@seznam.cz>
2023-02-13 22:48:44 +0100

    Implement query validator tests

--- src/Tests/UnitTests/File.Core.UnitTests/Validation/ConvertToQueryValidatorTests.cs
+++ src/Tests/UnitTests/File.Core.UnitTests/Validation/ConvertToQueryValidatorTests.cs
@@ -1,12 +1,128 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using File.Core.Abstractions;
+using File.Core.Queries;
+using File.Core.UnitTests.Assets;
+using File.Core.Validation;
+using File.Domain.Abstractions;
+using File.Domain.Queries;
+using Validot;
+using Validot.Results;
 
 namespace File.Core.UnitTests.Validation
 {
     public class ConvertToQueryValidatorTests
     {
+        private readonly Mock<IValidator<ConvertToQuery>> _convertToQueryValidatorMock;
+        private readonly Mock<IFileByOptionsValidator> _fileByOptionsValidatorMock;
+
+        private readonly Mock<IValidationResult> _validationResultMock;
+
+        private readonly IConvertToQueryValidator _uut;
+
+        public ConvertToQueryValidatorTests()
+        {
+            _convertToQueryValidatorMock = new Mock<IValidator<ConvertToQuery>>();
+            _fileByOptionsValidatorMock = new Mock<IFileByOptionsValidator>();
+
+            _validationResultMock = new Mock<IValidationResult>();
+
+            _uut = new ConvertToQueryValidator(_convertToQueryValidatorMock.Object, _fileByOptionsValidatorMock.Object);
+        }
+
+        [Fact]
+        public void QueryValidation_Failed()
+        {
+            //Arrange
+            var request = new ConvertToQuery(new Mock<IFile>().Object, string.Empty);
+
+            _convertToQueryValidatorMock.Setup(x => x.Validate(It.IsAny<ConvertToQuery>(), It.IsAny<bool>())).Returns(_validationResultMock.Object);
+            _validationResultMock.SetupGet(x=>x.AnyErrors).Returns(true);
+
+            //Act
+            var result = _uut.Validate(request);
+
+            //Assert
+            Assert.True(result.IsFailed);
+            _convertToQueryValidatorMock.Verify(x => x.Validate(It.Is<ConvertToQuery>(y=>y.Equals(request)), It.IsAny<bool>()), Times.Once);
+            _validationResultMock.VerifyGet(x => x.AnyErrors, Times.Once);
+        }
+
+        [Fact]
+        public void FileValidation_Failed()
+        {
+            //Arrange
+            var request = new ConvertToQuery(new Mock<IFile>().Object, string.Empty);
+
+            var failedMessage = "failedMessage";
+
+            _convertToQueryValidatorMock.Setup(x => x.Validate(It.IsAny<ConvertToQuery>(), It.IsAny<bool>())).Returns(_validationResultMock.Object);
+            _validationResultMock.SetupGet(x => x.AnyErrors).Returns(false);
+            _fileByOptionsValidatorMock.Setup(x => x.Validate(It.IsAny<IFile>())).Returns(Result.Fail(failedMessage));
+
+            //Act
+            var result = _uut.Validate(request);
+
+            //Assert
+            Assert.True(result.IsFailed);
+            Assert.Single(result.Errors);
+            Assert.Equal(failedMessage, result.Errors.First().Message);
+            _convertToQueryValidatorMock.Verify(x => x.Validate(It.Is<ConvertToQuery>(y => y.Equals(request)), It.IsAny<bool>()), Times.Once);
+            _validationResultMock.VerifyGet(x => x.AnyErrors, Times.Once);
+            _fileByOptionsValidatorMock.Verify(x => x.Validate(It.IsAny<IFile>()), Times.Once);
+        }
+
+        [Fact]
+        public void ConversionValidation_Failed()
+        {
+            //Arrange
+            var sourceExtension = "json";
+            var destinationExtension = "xml";
+            var fileMockOne = FileMockFactory.CreateMock(new byte[10], "application/json", $"resultFileOneName.{sourceExtension}");
+            var request = new ConvertToQuery(fileMockOne.Object, destinationExtension);
+
+            var failedMessage = "failedMessage";
+
+            _convertToQueryValidatorMock.Setup(x => x.Validate(It.IsAny<ConvertToQuery>(), It.IsAny<bool>())).Returns(_validationResultMock.Object);
+            _validationResultMock.SetupGet(x => x.AnyErrors).Returns(false);
+            _fileByOptionsValidatorMock.Setup(x => x.Validate(It.IsAny<IFile>())).Returns(Result.Ok(true));
+            _fileByOptionsValidatorMock.Setup(x => x.ValidateConversion(It.IsAny<string>(), It.IsAny<string>())).Returns(Result.Fail(failedMessage));
+
+            //Act
+            var result = _uut.Validate(request);
+
+            //Assert
+            Assert.True(result.IsFailed);
+            Assert.Single(result.Errors);
+            Assert.Equal(failedMessage, result.Errors.First().Message);
+            _convertToQueryValidatorMock.Verify(x => x.Validate(It.Is<ConvertToQuery>(y => y.Equals(request)), It.IsAny<bool>()), Times.Once);
+            _validationResultMock.VerifyGet(x => x.AnyErrors, Times.Once);
+            _fileByOptionsValidatorMock.Verify(x => x.Validate(It.IsAny<IFile>()), Times.Once);
+            _fileByOptionsValidatorMock.Verify(x => x.ValidateConversion(It.Is<string>(y => y.Equals(sourceExtension)), It.Is<string>(y => y.Equals(destinationExtension))), Times.Once);
+        }
+
+        [Fact]
+        public void Success()
+        {
+            //Arrange
+            var sourceExtension = "json";
+            var destinationExtension = "xml";
+            var fileMockOne = FileMockFactory.CreateMock(new byte[10], "application/json", $"resultFileOneName.{sourceExtension}");
+            var request = new ConvertToQuery(fileMockOne.Object, destinationExtension);
+
+            _convertToQueryValidatorMock.Setup(x => x.Validate(It.IsAny<ConvertToQuery>(), It.IsAny<bool>())).Returns(_validationResultMock.Object);
+            _validationResultMock.SetupGet(x => x.AnyErrors).Returns(false);
+            _fileByOptionsValidatorMock.Setup(x => x.Validate(It.IsAny<IFile>())).Returns(Result.Ok(true));
+            _fileByOptionsValidatorMock.Setup(x => x.ValidateConversion(It.IsAny<string>(), It.IsAny<string>())).Returns(Result.Ok(true));
+
+            //Act
+            var result = _uut.Validate(request);
+
+            //Assert
+            Assert.True(result.IsSuccess);
+            Assert.Empty(result.Errors);
+            _convertToQueryValidatorMock.Verify(x => x.Validate(It.Is<ConvertToQuery>(y => y.Equals(request)), It.IsAny<bool>()), Times.Once);
+            _validationResultMock.VerifyGet(x => x.AnyErrors, Times.Once);
+            _fileByOptionsValidatorMock.Verify(x => x.Validate(It.IsAny<IFile>()), Times.Once);
+            _fileByOptionsValidatorMock.Verify(x => x.ValidateConversion(It.Is<string>(y=>y.Equals(sourceExtension)), It.Is<string>(y=>y.Equals(destinationExtension))), Times.Once);
+        }
     }
 }
--- src/Tests/UnitTests/File.Core.UnitTests/Validation/ExportFileQueryValidatorTests.cs
+++ src/Tests/UnitTests/File.Core.UnitTests/Validation/ExportFileQueryValidatorTests.cs
@@ -1,12 +1,102 @@
-﻿using System;
+﻿using File.Core.Abstractions;
+using File.Core.Validation;
+using File.Domain.Queries;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Validot.Results;
+using Validot;
 
 namespace File.Core.UnitTests.Validation
 {
     public class ExportFileQueryValidatorTests
     {
+        private readonly Mock<IValidator<ExportFileQuery>> _exportFileQueryValidator;
+        private readonly Mock<IFileByOptionsValidator> _fileByOptionsValidator;
+
+        private readonly Mock<IValidationResult> _validationResultMock;
+
+        private readonly IExportFileQueryValidator _uut;
+
+        public ExportFileQueryValidatorTests()
+        {
+            _exportFileQueryValidator = new Mock<IValidator<ExportFileQuery>>();
+            _fileByOptionsValidator = new Mock<IFileByOptionsValidator>();
+
+            _validationResultMock = new Mock<IValidationResult>();
+
+            _uut = new ExportFileQueryValidator(_exportFileQueryValidator.Object, _fileByOptionsValidator.Object);
+        }
+
+        [Fact]
+        public void QueryValidation_Failed()
+        {
+            //Arrange
+            var request = new ExportFileQuery();
+
+            _exportFileQueryValidator.Setup(x => x.Validate(It.IsAny<ExportFileQuery>(), It.IsAny<bool>())).Returns(_validationResultMock.Object);
+            _validationResultMock.SetupGet(x => x.AnyErrors).Returns(true);
+
+            //Act
+            var result = _uut.Validate(request);
+
+            //Assert
+            Assert.True(result.IsFailed);
+            _exportFileQueryValidator.Verify(x => x.Validate(It.Is<ExportFileQuery>(y => y.Equals(request)), It.IsAny<bool>()), Times.Once);
+            _validationResultMock.VerifyGet(x => x.AnyErrors, Times.Once);
+        }
+
+        [Fact]
+        public void ExtensionValidation_Failed()
+        {
+            //Arrange
+            var request = new ExportFileQuery()
+            {
+                Extension = "ex"
+            };
+
+            var failedMessage = "failedMessage";
+
+            _exportFileQueryValidator.Setup(x => x.Validate(It.IsAny<ExportFileQuery>(), It.IsAny<bool>())).Returns(_validationResultMock.Object);
+            _validationResultMock.SetupGet(x => x.AnyErrors).Returns(false);
+            _fileByOptionsValidator.Setup(x => x.Validate(It.IsAny<string>())).Returns(Result.Fail(failedMessage));
+
+            //Act
+            var result = _uut.Validate(request);
+
+            //Assert
+            Assert.True(result.IsFailed);
+            Assert.Single(result.Errors);
+            Assert.Equal(failedMessage, result.Errors.First().Message);
+            _exportFileQueryValidator.Verify(x => x.Validate(It.Is<ExportFileQuery>(y => y.Equals(request)), It.IsAny<bool>()), Times.Once);
+            _validationResultMock.VerifyGet(x => x.AnyErrors, Times.Once);
+            _fileByOptionsValidator.Verify(x => x.Validate(It.Is<string>(y => y.Equals(request.Extension))), Times.Once);
+        }
+
+        [Fact]
+        public void Success()
+        {
+            //Arrange
+            var request = new ExportFileQuery()
+            {
+                Extension = "ex"
+            };
+
+            _exportFileQueryValidator.Setup(x => x.Validate(It.IsAny<ExportFileQuery>(), It.IsAny<bool>())).Returns(_validationResultMock.Object);
+            _validationResultMock.SetupGet(x => x.AnyErrors).Returns(false);
+            _fileByOptionsValidator.Setup(x => x.Validate(It.IsAny<string>())).Returns(Result.Ok(true));
+
+            //Act
+            var result = _uut.Validate(request);
+
+            //Assert
+            Assert.True(result.IsSuccess);
+            Assert.Empty(result.Errors);
+            _exportFileQueryValidator.Verify(x => x.Validate(It.Is<ExportFileQuery>(y => y.Equals(request)), It.IsAny<bool>()), Times.Once);
+            _validationResultMock.VerifyGet(x => x.AnyErrors, Times.Once);
+            _fileByOptionsValidator.Verify(x => x.Validate(It.Is<string>(y => y.Equals(request.Extension))), Times.Once);
+        }
     }
 }
